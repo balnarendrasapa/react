d96f23b5500f4c649b5d59eede35d75b0b0844e8
Bal Narendra Sapa <bnsapa2000@gmail.com>
2023-11-29 18:53:37 -0500

    Update Content heading style

--- src/Content/Content.tsx
+++ src/Content/Content.tsx
@@ -2,7 +2,7 @@ const Content = () => {
 
     return (
         <>
-            <h1>Content</h1>
+            <h1 className="text-3xl font-bold underline">Content</h1>
             <p>Some text</p>
         </>
     )
